ca64ee522085bb23a49d5ba55efeaf48a9502c00
Thomas Jeong <tj78@duke.edu>
2021-03-11 16:57:54 -0500

    Copy of working python ecg file.

---
+++ localTest.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+from scipy.fftpack import fft, fftfreq
+
+
+def getData(filename):
+    """Reads and parses ECG data and returns as numpy array.
+
+    ECG data is given in two columns as time (s) and voltage (mV). This
+    function parses data to be passed to another function which can then
+    be passed to another function to plot.
+
+    Args:
+        filename (string): a .csv file containing the sample ECG data
+
+    Returns:
+        my_data (float array): a numpy array containing the time and voltage
+    """
+    ecg_data = np.genfromtxt(filename, delimiter=',')
+    np.asarray(ecg_data)
+    return ecg_data
+
+
+def frequencyFiltering(ecg_data):
+    time = ecg_data[:,0]
+    n = len(time)
+    dt = max(time/n)
+    v = ecg_data[:,1]
+    vf = np.fft.fft(v,n)
+    PSD = vf * np.conj(vf)/n
+    freq = (1/(n*dt)) * np.arange(n)
+    # L = np.arange(1,np.floor(n/2),dtype='int')
+    # plt.plot(freq,PSD,color='c',label='Noisy')
+    peaks = findPeaks(PSD)
+    for peak in peaks:
+        lowerBound = peak-30
+        if lowerBound < 0:
+            lowerBound = 0
+        upperBound = peak+30
+        if upperBound > len(freq):
+            upperBound = len(freq)
+        vf[lowerBound:upperBound] = 0
+    # plt.xlim(-20,400)
+    # plt.legend()
+    # plt.show()
+    # cutoffs = PSD < 0.0001*max(PSD[peaks])
+    # vf = cutoffs * vf
+    ffilt = np.fft.ifft(vf)
+    plt.plot(time, ffilt,'k', linewidth=0.2)
+    plt.xlim(5,10)
+    plt.show()
+    for index, value in enumerate(PSD):
+        if value > 0.2*any(PSD[peaks]):
+            PSD[index] = 0
+    # plt.plot(freq,PSD,'k',label='Clean')
+    # plt.xlim(-20,400)
+    # plt.legend()
+    # plt.show()
+    return np.stack((time,ffilt), axis=-1)
+
+    
+def findPeaks(y):
+    y = np.array(y)
+    peaks = []
+    for index, data in enumerate(y): # find all possible peaks
+        if index == 0:
+            pass
+        else:
+            if (y[index] >= y[index-1]) and (y[index] > y[index+1]):
+                peaks.append(index)
+    unwanted = []
+    for i in peaks: # trim minor peaks
+        for j in peaks:
+            if np.real(y[i]) < 0.1*np.real(y[j]):
+                unwanted.append(i)
+                break
+    for i in unwanted:
+        peaks.remove(i)
+    return peaks
+
+
+def plotECG(ecg_data):
+    dist = 2000
+    time = ecg_data[0:dist,0]
+    voltage = ecg_data[0:dist,1]
+    plt.plot(time, voltage, 'b-', linewidth=0.1)
+    plt.xlabel("Time")
+    plt.ylabel("y")
+    plt.title("ECG Data")
+    plt.figure(num=None, figsize=(8,6), dpi=800, facecolor='w',edgecolor='k')
+    #plt.show()
+
+
+def heartbeatTimes(data):
+    t = data[:,1]
+    v = data[:,2]
+    index = findPeaks(v)
+    return t[findPeaks]
+    
+    
+for i in range(32):
+    i = i + 1
+    filename = "test_data" + str(i) + ".csv"    
+    test_data = getData(filename)
+    filt_data = frequencyFiltering(test_data)
+    plotECG(filt_data)
